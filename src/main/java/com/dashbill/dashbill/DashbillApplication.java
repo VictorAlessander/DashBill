23d2cec177f42994fca9dc8ed21b1be8d8c1f547
Victor Alessander <alessander@protonmail.com>
2018-04-21 02:23:49 -0300

    First commit

---
+++ src/main/java/com/dashbill/dashbill/DashbillApplication.java
@@ -0,0 +1,12 @@
+package com.dashbill.dashbill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashbillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashbillApplication.class, args);
+	}
+}
